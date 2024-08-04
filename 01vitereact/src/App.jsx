af6160ac2b897db77e87fe247b1007b687dc96da
Sagar Chauhan <chauhansagarsingh21@gmail.com>
2024-08-04 21:40:57 +0530

    vite react

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,11 +1,14 @@
 import First from "./first"
 
 function App() {
+  const username = "code with sagar"
   return (
     <>
     <First/>
+    <h1>My first component lets {username}</h1>
     </>
   )
 }
 
 export default App
+ 
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,10 +1,42 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import {jsx as _jsx} from "react/jsx-runtime.js"
 
+function MyApp(){
+    return(
+        <div>
+            <h1>hello iam custom App</h1>
+        </div>
+    )
+}
+// const reactElement = {
+//     type: "a",
+//     props: { href: "https://google.com", target: "_blank" },
+//     children: "click me to visit google",
+//   };
+
+  const anotherElement = (
+    <a href="https://google.com" target='_blank'>
+        visit google
+    </a>
+  )
+  const anotherUser = "iam another user"
+
+const reactElement = React.createElement(
+    'a',
+    {href:'https://google.com',target : '_blank'},
+    'click me to visit google ',
+    anotherUser
+
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
-    <App />
+//    <MyApp/>
+//    MyApp()
+// anotherElement
+reactElement
+//  <App/> 
 
 )
